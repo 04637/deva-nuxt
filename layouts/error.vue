0bade6d68bf6151e26a415b97867c3bcb11b9c8f
04637 <04637@163.com>
2019-11-22 23:56:22 +0800

    错误页面调整

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,32 +1,30 @@
 <template>
   <v-app dark>
     <v-layout justify-center align-start style="margin-top:20vh">
-      <v-card max-width="600px" height="30vh" min-height="200px" class="pa-5">
-        <v-layout justify-center style="height: 100%">
-          <v-layout justify-space-between column>
-            <v-layout justify-center>
-              <div v-if="error.statusCode === 404">
-                <svg class="icon" style="width: 200px; height: 100px;">
-                  <use xlink:href="#icon-undraw_page_not_found_suk"></use>
-                </svg>
-              </div>
-              <h1 v-else class="display-3">
-                <strong>{{ error.statusCode }}</strong>
-              </h1>
-            </v-layout>
-            <v-layout justify-center>
-              <h3>
-                {{ error.statusCode === 404 ? pageNotFound : otherError }}
-              </h3>
-            </v-layout>
-            <v-layout justify-center align-end>
-              <v-btn to="/" width="100%" color="primary">
-                <strong>回到主页</strong>
-              </v-btn>
-            </v-layout>
+      <v-layout justify-center style="height: 100%">
+        <v-flex xs11 md6 lg4>
+          <v-layout justify-center>
+            <div v-if="error.statusCode === 404">
+              <svg class="icon" style="width: 200px; height: 100px;">
+                <use xlink:href="#icon-undraw_page_not_found_suk"></use>
+              </svg>
+            </div>
+            <h1 v-else class="display-3">
+              <strong>{{ error.statusCode }}</strong>
+            </h1>
           </v-layout>
-        </v-layout>
-      </v-card>
+          <v-layout justify-center class="mt-5">
+            <h3>
+              {{ error.statusCode === 404 ? pageNotFound : otherError }}
+            </h3>
+          </v-layout>
+          <v-layout justify-center align-end class="mt-5">
+            <v-btn to="/" width="100%" color="primary">
+              <strong>回到主页</strong>
+            </v-btn>
+          </v-layout>
+        </v-flex>
+      </v-layout>
     </v-layout>
   </v-app>
 </template>
