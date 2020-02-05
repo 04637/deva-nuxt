501f62a99c0f5ef815a430cbd5b3b5a4ee062468
04637 <04637@163.com>
2020-02-05 11:22:50 +0800

    用day.js替换moment.js 包体积减小600+KB

--- plugins/moment.js
+++ plugins/moment.js
@@ -1,4 +1,8 @@
-import * as moment from 'moment'
+// 参考 https://willbrowning.me/reducing-the-vendor-bundle-size-in-nuxt-js/ 优化包体积, 替换掉moment
 import Vue from 'vue'
-moment().locale('zh')
-Vue.filter('moment', (v) => moment(v).format('YYYY-MM-DD HH:mm:ss'))
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs')
+dayjs.locale('zh-cn')
+Vue.filter('moment', (v) => {
+  return dayjs(v).format('YYYY-M-D H:m:s')
+})
